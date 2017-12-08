83d8729a96c1f80cbc22c1ce02b140057d3042b5
carlosalmonte04 <vacarlos_alm@hotmail.com>
2017-12-08 00:25:27 -0500

    turn off debug mode + add allowed host

--- csv_parser/settings.py
+++ csv_parser/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '2%oc2^6**y=9q2!)2w5-kc&0l5q(8kgtv_r=9d0($v211869)('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1']
+ALLOWED_HOSTS = ['localhost','127.0.0.1', 'sf-csv-parser.herokuapp.com', 'sf-code-test.herokuapp.com']
 
 
 
